4b704983660217550fe17914f925d42ba2e2432c
andermax <maks@cateye.lv>
2020-10-12 19:43:01 +0300

    imap ssl expunge for deskpro with csv list

---
+++ text/text.py
@@ -0,0 +1,14 @@
+name = "makSims anderSons"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+text = " text "
+print(text)
+print(text.lstrip())
+print(text.rstrip())
+
+age = 39
+
+print(name.title()+" Age:"+str(age))
